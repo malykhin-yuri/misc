d30ddcf50384a928fb289fe18ef810b13dc3222b
Yuri Malykhin <malykhin-yuri@yandex.ru>
2025-05-27 14:56:43 +0300

    forgotten

---
+++ turing_machine/common.py
@@ -0,0 +1,14 @@
+class PrettyTape:
+    def __init__(self, tape, head):
+        self.tape = tape
+        self.head = head
+    def __repr__(self):
+        reprs = []
+        for index, item in enumerate(self.tape):
+            if index == self.head:
+                item_repr = '[{}]'.format(item)
+            else:
+                item_repr = '{:^3}'.format(str(item))
+            reprs.append(item_repr)
+        return '@{}: |'.format(self.head) + '|'.join(reprs) + '|'
+
